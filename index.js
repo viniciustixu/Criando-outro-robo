5d00009db0982549d837901c6baf1b3953cfe9d5
Vinicius Souza <120525353+viniciustixu@users.noreply.github.com>
2023-10-18 23:34:12 -0300

    Up

--- index.js
+++ index.js
@@ -15,61 +15,63 @@ async function clickLoadMore(page, selector, times) {
   }
 }
 
-
-
-async function scrapeProductData(page, browser) {
+async function scrapeProductData(page) {
   const productData = [];
   const productDivs = await page.$$('.css-rj8yxg');
 
-  const batchSize = 10; // Defina o tamanho do lote
-  const batches = [];
-
-  for (let i = 0; i < productDivs.length; i += batchSize) {
-    const batch = productDivs.slice(i, i + batchSize);
-    batches.push(batch);
-  }
-
-  for (const batch of batches) {
-    await Promise.all(batch.map(async (div) => {
-      const infoIcon = await div.$('img[aria-haspopup="dialog"]');
-      if (infoIcon) {
-        const textElement = await div.$('.chakra-text');
-        const priceElement = await div.$('.chakra-heading');
-        if (textElement && priceElement) {
-          const textValue = await textElement.evaluate(el => el.textContent);
-          const precoDoItem = await priceElement.evaluate(el => el.textContent);
-          const idDoItem = textValue.replace('#', '');
-          const link = `https://openloot.com/items/BT0/Hourglass_Common/issue/${idDoItem}`;
-
-          // Mova a parte de abrir a nova página e extrair informações para dentro do loop para evitar elementos desconectados.
-          const newPage = await browser.newPage();
-          await newPage.goto(link);
-
-          const tempo = await newPage.evaluate(() => {
-            const timeRemainingElement = document.querySelector("#__next > div > main > div > div > div > section > div > div.css-pckl1t > div.css-1nlrkd1 > div.chakra-stack.css-1yq6kto > div.css-12n3wqh > div > div > p.chakra-text.css-10ycfue");
-            if (timeRemainingElement) {
-              return timeRemainingElement.textContent;
-            }
-            return '';
-          });
-
-          await newPage.close();
-          if (tempo !== '0.00') {
-            productData.push({
-              id: idDoItem,
-              price: precoDoItem,
-              link: link,
-              time: tempo,
-            });
-          }
-        }
+  for (const div of productDivs) {
+    const infoIcon = await div.$('img[aria-haspopup="dialog"]');
+    if (infoIcon) {
+      const textElement = await div.$('.chakra-text');
+      const priceElement = await div.$('.chakra-heading');
+      if (textElement && priceElement) {
+        const textValue = await textElement.evaluate(el => el.textContent);
+        const precoDoItem = await priceElement.evaluate(el => el.textContent);
+        const idDoItem = textValue.replace('#', '');
+        productData.push({
+          id: idDoItem,
+          price: precoDoItem,
+        });
       }
-    }));
+    }
   }
 
   return productData;
 }
 
+async function scrapeTimeRemaining(browser, productData, poolSize) {
+  const chunks = chunkArray(productData, poolSize);
+
+  for (const chunk of chunks) {
+    const promises = chunk.map(async (product) => {
+      const link = `https://openloot.com/items/BT0/Hourglass_Common/issue/${product.id}`;
+      const newPage = await browser.newPage(); // Correção aqui
+      await newPage.goto(link);
+      const tempo = await newPage.evaluate(() => {
+        const timeRemainingElement = document.querySelector("#__next > div > main > div > div > div > section > div > div.css-pckl1t > div.css-1nlrkd1 > div.chakra-stack.css-1yq6kto > div.css-12n3wqh > div > div > p.chakra-text.css-10ycfue");
+        if (timeRemainingElement) {
+          return timeRemainingElement.textContent;
+        }
+        return '';
+      });
+      await newPage.close();
+
+      if (tempo !== '0.00') {
+        product.time = tempo;
+      }
+    });
+
+    await Promise.all(promises);
+  }
+}
+
+function chunkArray(array, chunkSize) {
+  const chunks = [];
+  for (let i = 0; i < array.length; i += chunkSize) {
+    chunks.push(array.slice(i, i + chunkSize));
+  }
+  return chunks;
+}
 
 (async () => {
   const browser = await puppeteer.launch({ headless: false });
@@ -80,20 +82,25 @@ async function scrapeProductData(page, browser) {
   await page.goto('https://openloot.com/items/BT0/Hourglass_Common');
   await page.waitForNavigation({ waitUntil: 'load' });
   await page.waitForTimeout(5000);
-  await clickLoadMore(page, LoadMoreSelector, 100); //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+  await clickLoadMore(page, LoadMoreSelector, 50); // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
+  const productData = await scrapeProductData(page);
 
+  await page.close();
 
-  const productData = await scrapeProductData(page, browser); // Passe 'browser' como argumento
+  const poolSize = 10; 
+  await scrapeTimeRemaining(browser, productData, poolSize);
 
-  productData.sort((a, b) => parseFloat(a.time) - parseFloat(b.time));
+  const filteredProductData = productData.filter((product) => product.time && product.time !== '0.00');
+
+  filteredProductData.sort((a, b) => parseFloat(a.time) - parseFloat(b.time));
 
   let htmlTable = '<table>';
   htmlTable += '<thead><tr><th>ID</th><th>Preço</th><th>Link</th><th>Tempo</th></tr></thead>';
   htmlTable += '<tbody>';
 
-  productData.forEach((product) => {
-    htmlTable += `<tr><td>${product.id}</td><td>${product.price}</td><td><a href="${product.link}" target="_blank">Link</a></td><td>${product.time}</td></tr>`;
+  filteredProductData.forEach((product) => {
+    htmlTable += `<tr><td>${product.id}</td><td>${product.price}</td><td><a href="https://openloot.com/items/BT0/Hourglass_Common/issue/${product.id}" target="_blank">Link</a></td><td>${product.time}</td></tr>`;
   });
 
   htmlTable += '</tbody></table>';
@@ -102,7 +109,7 @@ async function scrapeProductData(page, browser) {
   const fs = require('fs').promises;
   await fs.writeFile('index.html', htmlTable);
 
-  console.log(productData);
+  console.log(filteredProductData);
 
   await browser.close();
 })();
